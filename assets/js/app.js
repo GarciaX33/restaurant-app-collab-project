ccfe8289e2abddff0cfc6aeefd5d7f270baaa74c
GarciaX33 <danny.garcia80@hotmail.com>
2019-06-26 11:19:24 -0500

    will add and edit cards and images

--- assets/js/app.js
+++ assets/js/app.js
@@ -20,16 +20,59 @@ function displayRestaurantInfo() {
 
             var address = [br[i].restaurant.location.latitude, br[i].restaurant.location.longitude, br[i].restaurant.name]
 
-            locations.push(address) // console.log(br[i].restaurant.location.latitude)
-            // console.log(br[i].restaurant.location.longitude)
-            // , res[i].restaurant.location.longitude];
-            // locations.push(res[i].restaurant.location.latitude)
+            locations.push(address) 
         }
-        $(".bottomCard").append("<div><img src=" + res.best_rated_restaurant[3].restaurant.photos[1].photo.url + "><div>");
-        $(".bottomCard").append("<div class='restName'><strong>" + res.best_rated_restaurant[3].restaurant.name + "</strong> <div>");
-        $(".bottomCard").append("<div class='restName'>" + res.best_rated_restaurant[3].restaurant.location.address + "<div>");
-        $(".bottomCard").append("<div class='bottomCardButton'><button>View Details</button><div>");
-        // $(".bottomCard").append("<div><img src="+res.best_rated_restaurant[2].restaurant.featured_image+"><div>");
+        // Card 1
+        $(".bottomCard1").append("<div><img src=" + br[0].restaurant.photos[0].photo.url + "><div>");
+        $(".bottomCard1").append("<div class='restName'><strong>" + br[0].restaurant.name + "</strong> <div>");
+        $(".bottomCard1").append("<div class='restName'>" + br[0].restaurant.location.address + "<div>");
+        $(".bottomCard1").append("<div class='bottomCardButton'><button>View Details</button><div>");
+        // Card 2
+        $(".bottomCard2").append("<div><img src=" + br[1].restaurant.photos[1].photo.url + "><div>");
+        $(".bottomCard2").append("<div class='restName'><strong>" + br[1].restaurant.name + "</strong> <div>");
+        $(".bottomCard2").append("<div class='restName'>" + br[1].restaurant.location.address + "<div>");
+        $(".bottomCard2").append("<div class='bottomCardButton'><button>View Details</button><div>");
+        // Card 3
+        $(".bottomCard3").append("<div><img src=" + br[2].restaurant.photos[2].photo.url + "><div>");
+        $(".bottomCard3").append("<div class='restName'><strong>" + br[2].restaurant.name + "</strong> <div>");
+        $(".bottomCard3").append("<div class='restName'>" + br[2].restaurant.location.address + "<div>");
+        $(".bottomCard3").append("<div class='bottomCardButton'><button>View Details</button><div>");
+        // Card 4
+        $(".bottomCard4").append("<div><img src=" + br[3].restaurant.photos[3].photo.url + "><div>");
+        $(".bottomCard4").append("<div class='restName'><strong>" + br[3].restaurant.name + "</strong> <div>");
+        $(".bottomCard4").append("<div class='restName'>" + br[3].restaurant.location.address + "<div>");
+        $(".bottomCard4").append("<div class='bottomCardButton'><button>View Details</button><div>");
+        // Card 5
+        $(".bottomCard5").append("<div><img src=" + br[4].restaurant.photos[4].photo.url + "><div>");
+        $(".bottomCard5").append("<div class='restName'><strong>" + br[4].restaurant.name + "</strong> <div>");
+        $(".bottomCard5").append("<div class='restName'>" + br[4].restaurant.location.address + "<div>");
+        $(".bottomCard5").append("<div class='bottomCardButton'><button>View Details</button><div>");
+        // Card 6
+        $(".bottomCard6").append("<div><img src=" + br[5].restaurant.photos[5].photo.url + "><div>");
+        $(".bottomCard6").append("<div class='restName'><strong>" + br[5].restaurant.name + "</strong> <div>");
+        $(".bottomCard6").append("<div class='restName'>" + br[5].restaurant.location.address + "<div>");
+        $(".bottomCard6").append("<div class='bottomCardButton'><button>View Details</button><div>");
+        // Card 7
+        $(".bottomCard7").append("<div><img src=" + br[6].restaurant.photos[6].photo.url + "><div>");
+        $(".bottomCard7").append("<div class='restName'><strong>" + br[6].restaurant.name + "</strong> <div>");
+        $(".bottomCard7").append("<div class='restName'>" + br[6].restaurant.location.address + "<div>");
+        $(".bottomCard7").append("<div class='bottomCardButton'><button>View Details</button><div>");
+        // Card 8
+        $(".bottomCard8").append("<div><img src=" + br[7].restaurant.photos[7].photo.url + "><div>");
+        $(".bottomCard8").append("<div class='restName'><strong>" + br[7].restaurant.name + "</strong> <div>");
+        $(".bottomCard8").append("<div class='restName'>" + br[7].restaurant.location.address + "<div>");
+        $(".bottomCard8").append("<div class='bottomCardButton'><button>View Details</button><div>");
+        // Card 9
+        $(".bottomCard9").append("<div><img src=" + br[8].restaurant.photos[8].photo.url + "><div>");
+        $(".bottomCard9").append("<div class='restName'><strong>" + br[8].restaurant.name + "</strong> <div>");
+        $(".bottomCard9").append("<div class='restName'>" + br[8].restaurant.location.address + "<div>");
+        $(".bottomCard9").append("<div class='bottomCardButton'><button>View Details</button><div>");
+        // Card 10
+        $(".bottomCard10").append("<div><img src=" + br[9].restaurant.photos[9].photo.url + "><div>");
+        $(".bottomCard10").append("<div class='restName'><strong>" + br[9].restaurant.name + "</strong> <div>");
+        $(".bottomCard10").append("<div class='restName'>" + br[9].restaurant.location.address + "<div>");
+        $(".bottomCard10").append("<div class='bottomCardButton'><button>View Details</button><div>");
+        
     });
 
 };
